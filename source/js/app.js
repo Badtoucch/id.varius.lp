d5b61b750fbe354d87c244d4089d1d32291efb6b
Vladislad T <v.trundenko@everad.com>
2019-01-31 18:21:12 +0200

    vlad

---
+++ source/js/app.js
@@ -0,0 +1,40 @@
+function revClose() {
+	let temper = $('.reviews-content-box');
+	for (let i = 0; i < temper.length; i++) {        temper[i].classList.remove('content-plus');    }
+}
+revClose();
+$('.reviews-content').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+	revClose();
+});
+$('.reviews-content-box__cbtn').on('click', function () {
+	this.parentElement.parentElement.classList.toggle("content-plus");
+});
+$('.reviews-content').slick({
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	arrows: true,
+	responsive: [
+	{
+		breakpoint: 1024,
+		settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		}
+	},
+	{
+		breakpoint: 767,
+		settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		}
+	},
+	{
+		breakpoint: 640,
+		settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1
+		}
+	}
+]
+});
